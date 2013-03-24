72fd21240476179e27008cca3fbae93a559e23e2
stargt <stargt@stargt.com>
2013-03-24 11:16:40 +0900

    최근꺼요

--- js/meeting.js
+++ js/meeting.js
@@ -3,6 +3,8 @@ var _tool_list_count = 0;
 var _tool_postit_count = 0;
 var _tool_mindmap_count = 0;
 var _tool_vote_count = 0;
+var _common_window_top = 50;
+var _common_windot_left = 20;
 
 $(document).ready(function() {
 	// 최초에는 오른쪽 패널에 참가자 탭을 보여줌
@@ -52,12 +54,12 @@ function addTool(type)
 		tool['title'] = '리스트 ' + _tool_list_count;
 		tool['width'] = list_window_width;
 		tool['height'] = list_window_height;
-		tool['left'] = 20;
-		tool['top'] = 20;
-		tool['source'] = '<div id="container" class="listtool">';
-			tool['source'] += '<section id="main_section">';
+		tool['left'] = _common_windot_left;
+		tool['top'] = _common_window_top;
+		tool['source'] = '<div id="container" class="listtool" style="height: ' + tool['height'] + 'px; width: ' + tool['width'] + 'px; overflow-y: scroll">';
+			tool['source'] += '<section id="main_section" style="height: ' + tool['height'] + 'px; width: ' + tool['width'] + 'px">';
 				tool['source'] += '<article>';
-				tool['source'] += '<header>';
+/*				tool['source'] += '<header>';
 					tool['source'] += '<div class="home_line">';
 						tool['source'] += '<div class="home_box">';
 							tool['source'] += '<div id="homeButton"><h2>Home</h2></div>';
@@ -66,7 +68,7 @@ function addTool(type)
 							tool['source'] += '<div id="initButton" onClick="initData()"><h2>Clear</h2></div>';
 						tool['source'] += '</div>';
 					tool['source'] += '</div>';
-				tool['source'] += '</header>';
+				tool['source'] += '</header>';*/
 				tool['source'] += '<div class="list_space">';
 					tool['source'] += '<div class="children">';
 					tool['source'] += '</div>';
@@ -82,9 +84,9 @@ function addTool(type)
 		tool['title'] = '포스트잇 ' + _tool_postit_count;
 		tool['width'] = postit_window_width;
 		tool['height'] = postit_window_height;
-		tool['left'] = 20;
-		tool['top'] = 20;
-		tool['source'] = '<iframe src="../postit/index.html" width="100%" height="100%"></iframe>';	// 포스트잇 도구 소스 들어갈 부분		
+		tool['left'] = _common_windot_left;
+		tool['top'] = _common_window_top;
+		tool['source'] = '<iframe src="../postit/index.html" width="' + tool['width'] + '" height="' + tool['height'] + '"></iframe>';	// 포스트잇 도구 소스 들어갈 부분		
 		break;
 	case "mindmap":
 		_tool_mindmap_count++;
@@ -93,8 +95,8 @@ function addTool(type)
 		tool['title'] = '마인드맵 ' + _tool_mindmap_count;
 		tool['width'] = mindmap_window_width;
 		tool['height'] = mindmap_window_height;
-		tool['left'] = 20;
-		tool['top'] = 20;
+		tool['left'] = _common_windot_left;
+		tool['top'] = _common_window_top;
 		tool['source'] = '<div id="main">';
 			tool['source'] += '<div id="jinomap" tabindex="-1" style="outline:none; border: none; position:relative; overflow:hidden; background:#f4f4f4;">';
 			tool['source'] += '</div>';
@@ -107,11 +109,13 @@ function addTool(type)
 		tool['title'] = '투표 ' + _tool_vote_count;
 		tool['width'] = vote_window_width;
 		tool['height'] = vote_window_height;
-		tool['left'] = 20;
-		tool['top'] = 20;
+		tool['left'] = _common_windot_left;
+		tool['top'] = _common_window_top;
 		tool['source'] = '';	// 투표 도구 소스 들어갈 부분
 		break;
 	}
+	_common_windot_left += 10;
+	_common_window_top += 10;
 	
 		console.log('addTool source : ', tool['source']);
 	
@@ -122,16 +126,21 @@ function addTool(type)
 // 도구창 보여주는 함수
 function showToolWindow(idx)
 {
+	var titlebarHeight = 29;
 	var toolname = _toolWindowList[idx]['name'];
-	var toolsource = '<div class="toolwindow" id="' + toolname + '">';
+	var tooltitle = _toolWindowList[idx]['title'];
+	var tooltop = _toolWindowList[idx]['top'];
+	var toolleft = _toolWindowList[idx]['left'];
+	var toolsource = '<div class="toolwindow" id="' + toolname + '"><div>';
+	toolsource += '<div class="title"><div class="title_text">' + tooltitle + '</div><div class="closewindow" onclick="closeToolWindow(\'' + idx + '\')">닫기</div><div class="clearboth"></div></div>';
 	var toolwidth = _toolWindowList[idx]['width'];
-	var toolheight = _toolWindowList[idx]['height'];
+	var toolheight = _toolWindowList[idx]['height'] + titlebarHeight;
 	
 	switch (_toolWindowList[idx]['type'])
 	{
 	case "list":		
 		toolsource += _toolWindowList[idx]['source'];
-		$('#content').append(toolsource);
+		$('#meetingboard').append(toolsource);
 	//	$('.' + _toolWindowList[idx]).show();
 	
 		newListTool();
@@ -140,26 +149,78 @@ function showToolWindow(idx)
 		break;
 	case "postit":
 		toolsource += _toolWindowList[idx]['source'];
-		$('#content').append(toolsource);
+		$('#meetingboard').append(toolsource);
 		$('#' + toolname).css('width', toolwidth);
 		$('#' + toolname).css('height', toolheight);
 		break;
 	case "mindmap":
 		toolsource += _toolWindowList[idx]['source'];
-		$('#content').append(toolsource);
+		$('#meetingboard').append(toolsource);
 		
 		newMindmapTool();
 		resizeMindmap(toolwidth, toolheight);
 		break;
 	}
-	toolsource += '</div>';
+	toolsource += '</div></div>';
 		
 	$('#' + _toolWindowList[idx]['name']).draggable();
+	$('#' + _toolWindowList[idx]['name']).css('left', toolleft + 'px');
+	$('#' + _toolWindowList[idx]['name']).css('top', tooltop + 'px');
+	
 	console.log('toolsource : ' + toolsource);
 }
 
 // 도구창 닫는 함수
 function closeToolWindow(idx)
 {
-	$('.' + _toolWindowList[idx]).hide();
+	$('#' + _toolWindowList[idx].name).hide();
+}
+
+
+// 팝업창 추가하고 여는 함수
+var _popupcount = 0;
+function showPopupWindow(content)
+{
+	var nowpopupcount = _popupcount;
+	var source = '<div id="popup' + nowpopupcount + '" class="splashpopup">';
+		source += '<div class="title"><span class="closepopup" onclick="closePopupWindow(' + nowpopupcount + ')">닫기</span></div>';
+		source += '<div class="popupcontent">' + content + '</div>';
+		source += '</div>';
+	$('#splashpopup_wrapper').append(source);
+	$('#popup' + nowpopupcount).fadeIn('slow');
+	
+	setTimeout("closePopupWindow(" + nowpopupcount + ")", 3000);
+	_popupcount++;
+}
+showPopupWindow("주문하신 상품의 결제가 완료되었습니다.");
+
+function closePopupWindow(idx)
+{
+	$('#popup' + idx).fadeOut('slow');
+}
+
+setInterval("showRunTime()", 1000);
+var _runTime = 0;
+var _totalTime = "30:00:00";	// 회의 전체 시간
+function showRunTime()
+{
+	var hour = parseInt(_runTime / 60 / 60);
+	var minute = parseInt(_runTime / 60);
+	var second = parseInt(_runTime % 60);
+	var tHour, tMinute, tSecond;
+	
+	if (hour < 10)	tHour = "0" + hour;
+	else	tHour = tHour;
+	
+	if (minute < 10)	tMinute = "0" + minute;
+	else	tMinute = minute;
+	
+	if (second < 10)	tSecond = "0" + second;
+	else	tSecond = second;
+	
+	var nowRunTime = tHour + ":" + tMinute + ":" + tSecond;
+	
+	$('#runTime').html(nowRunTime + " / " + _totalTime);
+	
+	_runTime++;
 }

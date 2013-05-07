b8fe41dbe3e732eb2b4e4885fa8e0c853f6400d5
Jeong <scvjeong@gmail.com>
2013-05-07 17:04:30 +0900

    20130507_v1

--- routes/meeting.js
+++ routes/meeting.js
@@ -1,3 +1,11 @@
+var mysql_conn = require('../sql/mysql_server').mysql_conn;
+var EventEmitter = require('events').EventEmitter;
+var util = require('../lib/util');
+var check = require('validator').check,
+    sanitize = require('validator').sanitize;
+
+var _COMPLETE_FLAG_CNT = 1;
+
 exports.main = function(req, res){
 	/** session start **/
 	if( !req.session.email || !req.session.email.length )
@@ -18,9 +26,24 @@ exports.appraisal = function(req, res){
 
 exports.result = function(req, res){
 	/** session start **/
-	if( !req.session.email || !req.session.email.length )
-		res.redirect("/");
+	//if( !req.session.email || !req.session.email.length )
+	//	res.redirect("/");
 	/** session end **/
 
-	res.render('result', { title: 'Express' });
+	var evt = new EventEmitter();
+	var dao_c = require('../sql/common');
+	var dao_m = require('../sql/meeting');
+	var params = {};
+	var result = { meeting_result:{} };
+	var complete_flag = 0;
+
+	dao_m.dao_get_meeting_result(evt, mysql_conn, params);
+	evt.on('get_meeting_result', function(err, rows){
+		if(err) throw err;
+		result.meeting_result = rows;
+		console.log(rows);
+		complete_flag++;
+		if( complete_flag === _COMPLETE_FLAG_CNT )
+			res.render('meeting_result', {result:result} );
+	});
 };
---
+++ sql/meeting.js
@@ -0,0 +1,42 @@
+// get_meeting_result
+// params['idx_meeting']
+// params['idx_group']
+exports.dao_get_meeting_result = function(evt, mysql_conn, params){
+	params['idx_meeting'] = 60;
+	params['idx_group'] = 1;
+	
+	var sql = "SELECT ";
+	sql += "`A`.`idx`, ";
+	sql += "`A`.`subject`, ";
+	sql += "`A`.`goal`, ";
+	sql += "`A`.`start_time`, ";
+	sql += "`A`.`end_time`, ";
+	sql += "`B`.`subject` AS `agenda_subject`, ";
+	sql += "`B`.`goal` AS `agenda_goal`, ";
+	sql += "`B`.`start_time` AS `agenda_start_time`, ";
+	sql += "`B`.`end_time` AS `agenda_end_time`, ";
+	sql += "`B`.`order`, ";
+	sql += "`E`.`satisfaction`, ";
+	sql += "`E`.`ft_appraisal`, ";
+	sql += "`E`.`mvp`, ";
+	sql += "GROUP_CONCAT( DISTINCT `D`.`name` ORDER BY `D`.`name` ASC SEPARATOR ', ') AS `user_list`, ";
+	sql += "GROUP_CONCAT( `C`.`appraisal` ORDER BY `D`.`name` ASC SEPARATOR ', ') AS `user_appraisal` ";
+	sql += "FROM `meeting_planning` AS `A` ";
+	sql += "INNER JOIN `agenda` AS `B` ";
+	sql += "ON `A`.`idx` = `B`.`idx_meeting_planning` ";
+	sql += "INNER JOIN `relation_user_meeting` AS `C` ";
+	sql += "ON `A`.`idx` = `C`.`idx_meeting` ";	
+	sql += "INNER JOIN `user` AS `D` ";
+	sql += "ON `C`.`idx_user` = `D`.`idx` ";
+	sql += "INNER JOIN `meeting_appraisal` AS `E` ";
+	sql += "ON `A`.`idx` = `E`.`idx_meeting` ";
+	sql += "WHERE `A`.`idx` = '"+params['idx_meeting']+"' ";
+	sql += "AND `A`.`idx_owner` = '"+params['idx_group']+"' ";
+	sql += "GROUP BY `B`.`idx` ";
+	sql += "ORDER BY `B`.`order` ASC";
+	console.log(sql);
+	var query = mysql_conn.query(sql, function(err, rows, fields) {
+		evt.emit('get_meeting_result', err, rows);
+	});
+	return sql;
+}

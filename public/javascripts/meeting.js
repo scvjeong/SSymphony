857a92805b6ae88e244c7e6b8541fd067548f659
Go Dong Hyun <godong9@gmail.com>
2013-06-09 23:18:54 +0900

    20130609_V2_godong

--- public/javascripts/meeting.js
+++ public/javascripts/meeting.js
@@ -34,6 +34,27 @@ $(document).ready(function() {
 	// 크기 조정
 	$(window).resize();
 
+	
+	// 오디오 관련 초기화
+	
+	 try {
+      // webkit shim
+      window.AudioContext = window.AudioContext || window.webkitAudioContext;
+      navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia;
+      window.URL = window.URL || window.webkitURL;
+      
+      audio_context = new AudioContext;
+      console.log('Audio context set up.');
+      console.log('navigator.getUserMedia ' + (navigator.getUserMedia ? 'available.' : 'not present!'));
+    } catch (e) {
+      alert('No web audio support in this browser!');
+    }
+    
+    navigator.getUserMedia({audio: true}, startUserMedia, function(e) {
+      console.log('No live audio input: ' + e);
+    });
+
+
 	setRightpanel("participants");	// 최초에는 오른쪽 패널에 참가자 탭을 보여줌
 
 	// 소켓 열기
@@ -1721,3 +1742,64 @@ function noticeBarMoving()
 		noticeBarMoving();
 	});
 }
+
+
+/*
+오디오 녹음 부분
+
+*/
+var audio_context;
+var recorder;
+
+
+  function startUserMedia(stream) {
+	
+    var input = audio_context.createMediaStreamSource(stream);
+    console.log('Media stream created.');
+    
+    input.connect(audio_context.destination);
+    console.log('Input connected to audio context destination.');
+    
+    recorder = new Recorder(input);
+    console.log('Recorder initialised.');
+  }
+
+  function startRecording() {
+    recorder && recorder.record();
+    $('.record-btn').css("display", "none");
+	$('.stop-btn').css("display", "block");
+    console.log('Recording...');
+  }
+
+  function stopRecording() {
+    recorder && recorder.stop();
+    $('.record-btn').css("display", "block");
+	$('.stop-btn').css("display", "none");
+    console.log('Stopped recording.');
+    
+    // create WAV download link using audio data blob
+    createDownloadLink();
+    
+    recorder.clear();
+  }
+
+  function createDownloadLink() {
+    recorder && recorder.exportWAV(function(blob) {
+      var url = URL.createObjectURL(blob);
+      var li = document.createElement('li');
+      var au = document.createElement('audio');
+      var hf = document.createElement('a');
+      
+      au.controls = true;
+      au.src = url;
+      hf.href = url;
+      hf.download = new Date().toISOString() + '.wav';
+      hf.innerHTML = hf.download;
+      li.appendChild(au);
+      li.appendChild(hf);
+      recordingslist.appendChild(li);
+    });
+  }
+
+  
+	
